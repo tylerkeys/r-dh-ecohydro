a434acf13fc18e1b877f3819c1fab79203139f96
Robert Burgholzer <rburghol@vt.edu>
2017-09-13 15:01:45 -0400

    Restored secure token.

--- elf_rest_token.R
+++ elf_rest_token.R
@@ -4,7 +4,8 @@ elf_rest_token <- function(site, token){
   
 #Cross-site Request Forgery Protection (Token required for POST and PUT operations)
 csrf_url <- paste(site,"/restws/session/token/",sep="");
-csrf <- GET(url=csrf_url,authenticate("restws_admin","@dmin123RESTFUL"));
+source("./rest_user.private");
+csrf <- GET(url=csrf_url,authenticate(rest_uname,rest_pw));
 token <- content(csrf);
 
 } #close function
