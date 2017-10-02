12be241b4839054125e5efb4ccebc522b64567a7
Robert Burgholzer <rburghol@vt.edu>
2017-10-02 14:53:35 -0400

    Added IHA script and vahydro script functions.

---
+++ Analysis/fn_vahydro-1.0.R
@@ -0,0 +1,53 @@
+# Automating August Low Flows
+
+library('zoo')
+library('IHA')
+library(PearsonDS)
+
+
+fn_get_rundata <- function(elementid = -1, runid = -1, varname = 'Qout', scenid = 37) {
+  if (elementid == -1 ) {
+    return(FALSE);
+  }
+  if (runid == -1 ) {
+    return(FALSE);
+  }
+  # may be obsolete
+  #setInternet2(TRUE)
+  # Authentication
+  hash <- "4291a2a64734da6e0c223a77f4ac5b9f"
+  username <- "robertwb"
+  
+  # Set up query for batch of model objects
+  # Internal variable to construct the query
+  urlbase<-"http://deq1.bse.vt.edu/om/remote/get_modelData.php?elementid="
+  print(paste("Getting data for run ", runid, " for element ", elementid))      # creates the whole url by pasting the element and run ids into it
+  filename<-paste(urlbase, elementid, "&variables=", varname, "&runid=", runid, "&startdate=1984-10-01&enddate=2005-09-30", sep = "")
+  print(paste("From ", filename))
+  
+  dat = try(read.table(filename, header = TRUE, sep = ",")) 
+  if (class(dat)=='try-error') { 
+    # what to do if file empty 
+    print(paste("Error: empty file ", filename))
+    return (FALSE);
+  } else { 
+    #dat<-read.table(filename, header = TRUE, sep = ",")   #  reads the csv-formatted data from the url	
+    print(paste("Data obtained, found ", length(dat[,1]), " lines - formatting for IHA analysis"))
+    datv<-as.vector(dat)  # stores the data as a vector     
+    datv$thisdate <- as.POSIXct(datv$thisdate)
+    f3 <- zoo(datv$Qout, order.by = datv$thisdate)
+  }
+  return(f3);
+  
+}
+
+fn_storeprop_vahydro1 = function(){
+  # NOT FINISHED - JUST PASTED CODE
+  url <- "http://deq1.bse.vt.edu/om/remote/setModelData.php?hash="
+  print (paste("Setting 7Q10 for element ", id, " run id ", rid, " to ", x7q10 , sep = "") )
+  # building the correct url
+  ins_url <- paste(url, hash, "&username=", username, "&elementid=", id, "&runid=", rid, "&dataname=7q10&reporting_frequency=single&dataval=", x7q10, "&starttime=1984-10-01&endtime=2005-09-30&temporal_res=water_year", sep = "")  
+  #shell.exec(alf_url)  # opening the webpage
+  print(ins_url);
+  readLines(ins_url)
+}
