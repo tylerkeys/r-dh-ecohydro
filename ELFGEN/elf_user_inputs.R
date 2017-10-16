612e7653085cab9ed4b1ec090ad7da0a0fcc3cb6
COV\nrf46657 <joseph.kleiner@deq.virginia.gov>
2017-10-16 15:54:14 -0400

    various updates to ELFGEN files

--- ELFGEN/elf_user_inputs.R
+++ ELFGEN/elf_user_inputs.R
@@ -2,12 +2,12 @@ rm(list = ls())  #clear variables
 options(timeout=120); # set timeout to twice default level to avoid abort due to high traffic
  
 #----------------------------------------------
-site <- "http://deq1.bse.vt.edu/d.bet"    #Specify the site of interest, either d.bet OR d.dh
+site <- "http://deq1.bse.vt.edu/d.dh"    #Specify the site of interest, either d.bet OR d.dh
 #----------------------------------------------
 
 #----FOR RUNNING LOCALLY:
-fxn_locations <- "C:\\Users\\nrf46657\\Desktop\\elf_updates_9.27.17\\"          #Specify location of supporting function .R files
-save_directory <- "C:\\Users\\nrf46657\\Desktop\\elf_updates_9.27.17\\plots"  #Specify location for storing plot images locally
+fxn_locations <- "C:\\Users\\nrf46657\\Desktop\\VAHydro Development\\GitHub\\r-dh-ecohydro\\ELFGEN\\"          #Specify location of supporting function .R files
+save_directory <- "C:\\Users\\nrf46657\\Desktop\\VAHydro Development\\GitHub\\stash_plots"                     #Specify location for storing plot images locally
 
 #----FOR RUNNING FROM SERVER:
 #fxn_locations <- "/var/www/R/r-dh-ecohydro/"
@@ -29,30 +29,30 @@ inputs <- list(
   offset_hydrocode = 1,                     #Leave at 1 to start from begining of Watershed_Hydrocode for-loop
   pct_chg = 10,                             #Percent decrease in flow for barplots (keep at 10 for now)
   save_directory = save_directory, 
-  x_metric = c(
-#    'nhdp_drainage_sqkm'#,
-    'erom_q0001e_mean'
-#    'erom_q0001e_jan',
-#    'erom_q0001e_feb',
-#    'erom_q0001e_mar', 
-#    'erom_q0001e_apr', 
-#    'erom_q0001e_may',
-#    'erom_q0001e_june',
-#    'erom_q0001e_july',
-#    'erom_q0001e_aug',
-#    'erom_q0001e_sept',
-#    'erom_q0001e_oct',
-#    'erom_q0001e_nov',
-#    'erom_q0001e_dec'
+  not_x_metric = c(
+    'nhdp_drainage_sqkm',
+    'erom_q0001e_mean',
+    'erom_q0001e_jan',
+    'erom_q0001e_feb',
+    'erom_q0001e_mar', 
+    'erom_q0001e_apr', 
+    'erom_q0001e_may',
+    'erom_q0001e_june',
+    'erom_q0001e_july',
+    'erom_q0001e_aug',
+    'erom_q0001e_sept',
+    'erom_q0001e_oct',
+    'erom_q0001e_nov',
+    'erom_q0001e_dec'
   ),		
-  #x_metric = 'nhdp_drainage_sqkm', #Flow metric to be plotted on the x-axis
-  dis_y_metric = c(
+  x_metric = 'nhdp_drainage_sqkm', #Flow metric to be plotted on the x-axis
+  not_y_metric = c(
                'nhdp_drainage_sqkm',
                'aqbio_nt_bival',
                'aqbio_nt_cypr_native'
-              ),
+              ), #this can be used to process by multiple biometrics at once 
   y_metric = 'aqbio_nt_total',	   #Biometric to be plotted on the y-axis, see "dh variable key" column for options: https://docs.google.com/spreadsheets/d/1PnxY4Rxfk9hkuaXy7tv8yl-mPyJuHdQhrOUWx_E1Y_w/edit#gid=0
-  disabled_ws_ftype = c(
+  not_ws_ftype = c(
     'state',
     'hwi_region',
     'nhd_huc6',
@@ -62,16 +62,13 @@ inputs <- list(
     'ecoregion_iii',
     'ecoregion_iv',
     'ecoiii_huc6'
-  ),
+  ),#this can be used to process by multiple region types at once 
   ws_ftype = c('hwi_region'),		     #Options: state, hwi_region, nhd_huc8, nhd_huc6, ecoregion_iii, ecoregion_iv, ecoiii_huc6
-  target_hydrocode = 'atl_non_coastal_plain_usgs',           #Leave blank to process all, individual examples: usa_state_virginia for all of VA, atl_non_coastal_plain_usgs,ohio_river_basin_nhdplus,nhd_huc8_05050001...
+  target_hydrocode = '',           #Leave blank to process all, individual examples: usa_state_virginia for all of VA, atl_non_coastal_plain_usgs,ohio_river_basin_nhdplus,nhd_huc8_05050001...
   quantile = .80,                  #Specify the quantile to use for quantile regresion plots 
   xaxis_thresh = 15000,            #Leave at 15000 so all plots have idential axis limits 
- # startdate = '1600-01-01',        #Leave at 1600-01-01 when batch processing to encompass all sample dates (different date-ranges can be used for later analyses)
- # enddate = '2100-12-31',          #Leave at 2100-12-31 when batch processing to encompass all sample dates 
-
-  #analysis_timespan = '1990-2000', #used to subset data on date range 
-  analysis_timespan = 'full', #used to plot for entire timespan 
+  #analysis_timespan = '1990-2000',#used to subset data on date range 
+  analysis_timespan = 'full',      #used to plot for entire timespan 
 
   send_to_rest = "YES",            #"YES" to push ELF statistic outputs to VAHydro
   station_agg = "max",             #Specify aggregation to only use the "max" NT value for each station or "all" NT values
@@ -86,8 +83,8 @@ inputs <- list(
   
   quantreg = "YES",  #Plot using quantile regression method (YES or NO)
   pw_it = "YES",     #Plot using breakpoint determined by piecewise iterative function (YES or NO)
-  ymax = "YES",      #Plot using breakpoint at x-value corresponding to max y-value (YES or NO)
-  twopoint = "YES",    #Plot using basic two-point ELF method (YES or NO)
+  ymax = "NO",       #Plot using breakpoint at x-value corresponding to max y-value (YES or NO)
+  twopoint = "NO",   #Plot using basic two-point ELF method (YES or NO)
   glo = 1,   # PWIT Breakpoint lower guess (sqmi/cfs)
   ghi = 530, # PWIT Breakpoint upper guess (sqmi/cfs) - also used as DA breakpoint for elf_quantreg method
              # ghi values determined from ymax analyses,  q25 = 72 
