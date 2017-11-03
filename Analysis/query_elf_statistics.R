26463312781294da78ad37a5911ecf0dec461954
Robert Burgholzer <rburghol@vt.edu>
2017-11-03 10:05:52 -0400

    Encapsulated elf dataset statistical summary retrieval into function dn_dh_elfstats()

--- Analysis/query_elf_statistics.R
+++ Analysis/query_elf_statistics.R
@@ -10,23 +10,25 @@
 # station_agg.................max, all
 # stat_quantreg_glo...........0, 1, 100...
 # stat_quantreg_ghi...........530, 1000...
-# featureid...................hwi_region, state, nhd_huc6, nhd_huc8, ecoregion_iii, ecoregion_iv, ecoiii_huc6...
+# feature_ftype...................hwi_region, state, nhd_huc6, nhd_huc8, ecoregion_iii, ecoregion_iv, ecoiii_huc6...
 # stat_quantreg_x.............nhdp_drainage_sqkm, erom_q0001e_mean, OR Leave empty to return stats for all x-metrics
 
 # Query Inputs -----------------------------------------------------
-    site <- "http://deq1.bse.vt.edu/d.dh"
-    ftype <- "fe_quantreg_pwit"
-    fstatus <- "active"
-    analysis_timespan <- "full"
-    stat_quantreg_y <- "aqbio_nt_total"
-    sampres <- "species"
-    stat_quantreg_qu <- 0.80
-    station_agg <- "max"
-    stat_quantreg_glo <- 1
-    stat_quantreg_ghi <- 530
-    featureid <- "hwi_region"
-    stat_quantreg_x <- "erom_q0001e_mean"
+fn_dh_elfstats <- function(
+    site = "http://deq1.bse.vt.edu/d.dh",
+    ftype = "fe_quantreg_pwit",
+    fstatus = "active",
+    analysis_timespan = "full",
+    stat_quantreg_y = "aqbio_nt_total",
+    sampres = "species",
+    stat_quantreg_qu = 0.80,
+    station_agg = "max",
+    stat_quantreg_glo = 1,
+    stat_quantreg_ghi = 530,
+    feature_ftype = "nhd_huc8",
+    stat_quantreg_x = "") {
 #------------------------------------------------------------------
-    elf_statistics <- paste(site,"export_elf_statistics",ftype,fstatus,analysis_timespan,stat_quantreg_y,sampres,stat_quantreg_qu,station_agg,stat_quantreg_glo,stat_quantreg_ghi,featureid,stat_quantreg_x,sep = "/");
-    elf_statistics <- read.table(elf_statistics,header = TRUE, sep = ",")
-    print(head(elf_statistics))
+    elf_statistics <- paste(site,"export_elf_statistics",ftype,fstatus,analysis_timespan,stat_quantreg_y,sampres,stat_quantreg_qu,station_agg,stat_quantreg_glo,stat_quantreg_ghi,feature_ftype,stat_quantreg_x,sep = "/");
+    elf_statistics <- read.table(elf_statistics,header = TRUE, sep = ",");
+    return (elf_statistics);
+}
