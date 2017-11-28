d65e5851207b55db448b514548c0e850b17898fa
Joey Kleiner <jkleiner@vt.edu>
2017-11-28 12:00:32 -0500

    updated elf_pct_chg.R to display various % water withdrawals

--- Analysis/use_fn_dh_elfstats.R
+++ Analysis/use_fn_dh_elfstats.R
@@ -3,8 +3,11 @@
 base_url <- "http://deq1.bse.vt.edu/d.dh/"
 
 #Set location of "fn_dh_elfstats" function
-path <- "/Users/nrf46657/Desktop/query_elf_stats/"
+path <- "/Users/nrf46657/Desktop/VAHydro Development/GitHub/r-dh-ecohydro/Analysis/"
 source(paste(path,"query_elf_statistics.R", sep = ""))
 
 dataframe <- fn_dh_elfstats(feature_ftype = 'ecoregion_iii', yvar = 'aqbio_benthic_nt_total', sampres = 'maj_fam_gen_spec')
-dataframe <- fn_dh_elfstats(feature_ftype = 'ecoregion_iii', yvar = 'aqbio_nt_total', xvar = 'nhdp_drainage_sqkm') 
+dataframe <- fn_dh_elfstats(feature_ftype = 'ecoregion_iii', stat_quantreg_glo=0, yvar = 'aqbio_nt_total', xvar = 'nhdp_drainage_sqkm', ftype = 'fe_quantreg') 
+dataframe$d530 <- dataframe$out_m * log(530.0) + dataframe$out_b
+dataframe$d1 <- dataframe$out_m * log(1.0) + dataframe$out_b
+dataframe$xatzero <- exp(-1.0 * (dataframe$out_b  / dataframe$out_m))
--- ELFGEN/internal/elf_pct_chg.R
+++ ELFGEN/internal/elf_pct_chg.R
@@ -8,42 +8,58 @@ elf_pct_chg <- function(pct_inputs = list()){
   sampres <- pct_inputs$sampres
   startdate <- pct_inputs$startdate
   enddate <- pct_inputs$enddate
- 
-  its <- seq(1, 500, 1)
-  pct <- (its -((pct_chg/100)*its))
-  sa <- (ruslope*log(its))+ruint
-  sb <- (ruslope*log(pct))+ruint
-  pct_chgb <- (((sa-sb)/sa)*100)
-  pct_chgs = c(pct_chgb)
+
+  its <- seq(0.1, 500, 0.01)
   xvalues = c(its)
+  sa <- (ruslope*log(its))+ruint
+  pct_list <- c(5,10,20,30,40,50)
+  slope_table <- data.frame(xvalues=xvalues,
+                            stringsAsFactors=FALSE)
   
-  slope_table = data.frame(xvalues,pct_chgs)
-  #print(head(slope_table))
+  #i <- 1
+  for (i in 1:length(pct_list)) {
+    pct <- (its -((pct_list[i]/100)*its))
+    sb <- (ruslope*log(pct))+ruint
+    pct_chgb <- (((sa-sb)/sa)*100)
+    pct_chgs = c(pct_chgb)
+    slope_table_i = data.frame(pct_chgs)
+    names(slope_table_i) <- c(paste("pct_chg_",pct_list[i],sep=""))
+    slope_table <- cbind(slope_table, slope_table_i)
+  }
   
 title_projname <- sampres
 
 #Plot titles
-ptitle <- paste("Change in ",biometric_title," at ",pct_chg,"% Flow Reduction","\n", Feature.Name," (",startdate," to ",enddate,")\n",title_projname," grouping",sep="")
+ptitle <- paste("Change in ",biometric_title," at Various % Flow Reductions","\n", Feature.Name," (",startdate," to ",enddate,")\n",title_projname," grouping",sep="")
 xaxis_title <- paste("\n",flow_title,sep="");
 yaxis_title <- paste("% Decrease in ", biometric_title,"\n", sep="");
 
-if(flow_title == "Drainage Area (km^2)"){
-	yup_lim <- 6
-} else {
-	yup_lim <- 4
-}
-
-
-plt2 <- ggplot(slope_table, aes(x=xvalues, y=pct_chgs)) + 
-  geom_line()+
+plt2 <- ggplot(slope_table, aes(x=xvalues, y=pct_chgs_20)) + 
+  
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_50,color = "black")) + 
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_40,color = "blue")) + 
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_30,color = "green"))+
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_20,color = "red"))+
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_10,color = "violet"))+
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_5,color = "wheat"))+
+  
   scale_color_manual(
-    "Legend",
-    values=c("red"),
-    labels=c("Analysis Point")
-  ) + 
+    "Flow Reduction",
+     values=c("black","blue","forestgreen","red","darkmagenta","sienna4"),
+     labels=c("50%","40%","30%","20%","10%","5%")
+    ) + 
+  
+  scale_x_log10(
+    limits = c(0.1,500),
+    breaks = trans_breaks("log10", function(x) {10^x})#,
+    #labels = trans_format("log10", math_format(10^.x))
+    #labels = round(x,digits = 0)
+  ) +
+  annotation_logticks(sides = "b")+
+  
   ggtitle(ptitle)+
   labs(x=xaxis_title,y=yaxis_title)+
-  theme(axis.text.x = element_text(colour="grey20",size=20,hjust=.5,vjust=.5,face="plain"),
+  theme(axis.text.x = element_text(colour="grey20",size=15,hjust=.5,vjust=.5,face="plain"),
         axis.text.y = element_text(colour="grey20",size=15,hjust=.5,vjust=.5,face="plain"))
 
 }
