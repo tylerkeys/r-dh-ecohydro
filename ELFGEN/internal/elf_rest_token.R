f65438a5569c146b6439b9c8da7455f24bd2fbfa
Joey Kleiner <jkleiner@vt.edu>
2018-01-19 10:02:30 -0500

    Update elf_rest_token.R

--- ELFGEN/internal/elf_rest_token.R
+++ ELFGEN/internal/elf_rest_token.R
@@ -5,7 +5,7 @@ elf_rest_token <- function(site, token){
 #Cross-site Request Forgery Protection (Token required for POST and PUT operations)
 csrf_url <- paste(site,"/restws/session/token/",sep="");
 #source("./rest_user.private");
-csrf <- GET(url=csrf_url,authenticate("restws_admin","@dmin123REST"));
+csrf <- GET(url=csrf_url,authenticate("PRIVATE","PRIVATE"));
 token <- content(csrf);
 
 } #close function
